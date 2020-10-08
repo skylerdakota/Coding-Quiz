b8076df98422821a4e4af9223de18012963ef41f
Skyler Dakota Lawton <skylerdakota@gmail.com>
2020-10-08 14:21:23 -0400

    attempt to resolve errors

--- script2.js
+++ script2.js
@@ -1,49 +1,49 @@
-var goBackBtn = document.querySelector("#goback");
-var clearHighScoreBtn = document.querySelector("#clearhighscore");
-var scoreLocation = document.querySelector("#scorelocation");
-var startQuizBtn = document.querySelector("#startquiz");
-var submitBtn = document.querySelector("#submit");
+// var goBackBtn = document.querySelector("#goback");
+// var clearHighScoreBtn = document.querySelector("#clearhighscore");
+// var scoreLocation = document.querySelector("#scorelocation");
+// var startQuizBtn = document.querySelector("#startquiz");
+// var submitBtn = document.querySelector("#submit");
 
 // on click event for final submission
-submitBtn.addEventListener("click", function (event) {
-    event.preventDefault();
+// submitBtn.addEventListener("click", function (event) {
+//     event.preventDefault();
 
-    var lastScore = document.querySelector("#scorelocation").value;
-    // set variable to collect value of initial input
-    var initials = initialsInput.value;
-    // redirect page to print text for collected initial input
-    //***append the score
-    if (scoreLocation = "") {
-        scoreLocation.textContent = ((window.location.href = "scores.html"), initials + " - " + score);
-    } else {
-        localStorage.setItem("scorelocation", lastScore);
-        renderLastWinner()
-        scoreLocation.textContent.append(initials + " - " + score)
-    }
-});
+//     var lastScore = document.querySelector("#scorelocation").value;
+//     // set variable to collect value of initial input
+//     var initials = initialsInput.value;
+//     // redirect page to print text for collected initial input
+//     //***append the score
+//     if (scoreLocation = "") {
+//         scoreLocation.textContent = ((window.location.href = "scores.html"), initials + " - " + score);
+//     } else {
+//         localStorage.setItem("scorelocation", lastScore);
+//         renderLastWinner()
+//         scoreLocation.textContent.append(initials + " - " + score)
+//     }
+// });
 
 // on click event for the back button to return to index.html
-goBackBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    // change windows to the start page
-    window.location.href = "index.html"
-});
+// goBackBtn.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     // change windows to the start page
+//     window.location.href = "index.html"
+// });
 
 // on click event for clearing the high score
-clearHighScoreBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    // set variable for collected values of initial inputs
-    var initials = initialsInputEl.value;
-    // clear text for collected initial inputs
-    // ***figure out how to clear local storage?
-    scoreLocation.textContent = clearInterval(initials);
-});
+// clearHighScoreBtn.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     // set variable for collected values of initial inputs
+//     var initials = initialsInputEl.value;
+//     // clear text for collected initial inputs
+//     // ***figure out how to clear local storage?
+//     scoreLocation.textContent = clearInterval(initials);
+// });
 
 // *** store answers and initials in local storage
-function renderLastWinner(){
-    var lastScore = localStorage.getItem("#scorelocation");
-    scoreLocation.textContent = lastScore;
-}
+// function renderLastWinner(){
+//     var lastScore = localStorage.getItem("#scorelocation");
+//     scoreLocation.textContent = lastScore;
+// }
 
 
 
